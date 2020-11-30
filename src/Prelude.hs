255fb69c864fb9877ab331a3923cc9e453bd3c0a
James King <james@agentultra.com>
2020-11-30 00:50:15 -0500

    Modify src/Prelude.hs
    Added some more helper functions.

--- src/Prelude.hs
+++ src/Prelude.hs
@@ -1,6 +1,10 @@
 module Prelude
   ( module Relude
-  , readNums
+  , pairs
+  , readDecimals
+  , readSignedDecimals
+  , readRationals
+  , readDoubles
   )
   where
 
@@ -8,5 +12,35 @@ import qualified Data.Text as T
 import qualified Data.Text.Read as T
 import Relude
 
-readNums :: Text -> Either String [Int]
-readNums t = map fst <$> traverse (T.decimal @Int) (T.split (== ',') t)
+-- | Read a comma-separated list of values
+readValuesAs :: T.Reader a -> Text -> Either String [a]
+readValuesAs reader txt = map fst <$> traverse reader (T.split (== ',') txt)
+
+-- | Parse a comma-separated list of @Int@
+readDecimals :: Text -> Either String [Int]
+readDecimals = readValuesAs T.decimal
+
+-- | Parse a comma-separated list of signed @Int@
+readSignedDecimals :: Text -> Either String [Int]
+readSignedDecimals = readValuesAs $ T.signed T.decimal
+
+-- | Parse a list of @Rational@
+readRationals :: Text -> Either String [Rational]
+readRationals = readValuesAs T.rational
+
+-- | Parse a list of @Double@
+readDoubles :: Text -> Either String [Double]
+readDoubles = readValuesAs T.double
+
+-- | Break a list into consecutive pairs with a possible remainder for
+-- uneven lists
+pairs :: [a] -> ([(a, a)], Maybe a)
+pairs xs
+  | null xs = ([], Nothing)
+  | even $ length xs = (go xs, Nothing)
+  | otherwise = bimap go listToMaybe $ splitAt (length xs - 1) xs
+  where
+    go :: [a] -> [(a, a)]
+    go [] = []
+    go [_] = []
+    go (x:y:xs) = (x, y) : go xs
