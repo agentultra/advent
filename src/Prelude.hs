6ba4f3184e19f0cfcf2c000fd057cd26b20ebe97
James King <james@agentultra.com>
2020-11-29 23:28:24 -0500

    Modify src/Prelude.hs
    Added the `readNums` function

--- src/Prelude.hs
+++ src/Prelude.hs
@@ -1,6 +1,12 @@
 module Prelude
   ( module Relude
+  , readNums
   )
   where
 
+import qualified Data.Text as T
+import qualified Data.Text.Read as T
 import Relude
+
+readNums :: Text -> Either String [Int]
+readNums t = map fst <$> traverse (T.decimal @Int) (T.split (== ',') t)
