fbad184dfbcf5d0ac8283fb2d47bb6374e377030
James King <james@agentultra.com>
2020-12-01 22:26:09 -0500

    Add solutions for Day1
    Hooray for list comprehensions.

--- src/Advent/Y2020/Day1/Part1.hs
+++ src/Advent/Y2020/Day1/Part1.hs
@@ -1,4 +1,14 @@
 module Advent.Y2020.Day1.Part1 where
 
+import Data.Text.Read as T
+
+import Advent.Input
+
 solution :: IO ()
-solution = putStrLn "Not implemented yet"
+solution = do
+  input <- readInput "./data/2020/Day1.txt"
+  let (Right nums) = map fst <$> traverse T.decimal input
+  let result = take 1 $ [ (x, y) | x <- nums, y <- nums, x + y == 2020]
+  case listToMaybe result of
+    Just (x, y) -> print $ x * y
+    Nothing     -> putStrLn "Nope"
--- src/Advent/Y2020/Day1/Part2.hs
+++ src/Advent/Y2020/Day1/Part2.hs
@@ -1,4 +1,14 @@
 module Advent.Y2020.Day1.Part2 where
 
+import Data.Text.Read as T
+
+import Advent.Input
+
 solution :: IO ()
-solution = putStrLn "Not implemented yet"
+solution = do
+  input <- readInput "./data/2020/Day1.txt"
+  let (Right nums) = map fst <$> traverse T.decimal input
+  let result = take 1 $ [ (x, y, z) | x <- nums, y <-nums, z <- nums, x + y + z == 2020]
+  case listToMaybe result of
+    Just (x, y, z) -> print $ x * y * z
+    Nothing -> putStrLn "Nope"
