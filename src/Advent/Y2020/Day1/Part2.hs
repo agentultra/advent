ac27bdc10ebbcbb0c506d962c57a7ff58d738e17
James King <james@agentultra.com>
2020-12-02 08:58:18 -0500

    Modify src/Advent/Y2020/Day1/Part2.hs
    I fixed this one up to use combinations as well.
    
    The problem with the prior solution, even though it won a star for my
    input, was that it was representing a permutation:
    
       { \A x,y \in Nums: x + y == 2020 }
    
    This formula means that the variables `x` and `y` could be the same
    element.

--- src/Advent/Y2020/Day1/Part2.hs
+++ src/Advent/Y2020/Day1/Part2.hs
@@ -4,11 +4,14 @@ import Data.Text.Read as T
 
 import Advent.Input
 
+combinations3 :: [a] -> [(a, a, a)]
+combinations3 xs = [ (x, y, z) | (x:ys) <- tails xs, (y:zs) <- tails ys, z <- zs ]
+
 solution :: IO ()
 solution = do
   input <- readInput "./data/2020/Day1.txt"
   let (Right nums) = map fst <$> traverse T.decimal input
-  let result = take 1 $ [ (x, y, z) | x <- nums, y <-nums, z <- nums, x + y + z == 2020]
+  let result = take 1 $ [ (x, y, z) | (x, y, z) <- combinations3 nums, x + y + z == 2020]
   case listToMaybe result of
     Just (x, y, z) -> print $ x * y * z
     Nothing -> putStrLn "Nope"
