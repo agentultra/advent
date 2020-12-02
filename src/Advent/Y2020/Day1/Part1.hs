61104616471071a9c526148fc47c1ddd1e8787b3
James King <james@agentultra.com>
2020-12-02 08:57:17 -0500

    Modify src/Advent/Y2020/Day1/Part1.hs
    Cleaned up the solution to use a combinations function which should
    replace the permutations I bodged before.

--- src/Advent/Y2020/Day1/Part1.hs
+++ src/Advent/Y2020/Day1/Part1.hs
@@ -4,11 +4,14 @@ import Data.Text.Read as T
 
 import Advent.Input
 
+combinations2 :: [a] -> [(a, a)]
+combinations2 xs = [(x, y) | (x:ys) <- tails xs, y <- ys]
+
 solution :: IO ()
 solution = do
   input <- readInput "./data/2020/Day1.txt"
   let (Right nums) = map fst <$> traverse T.decimal input
-  let result = take 1 $ [ (x, y) | x <- nums, y <- nums, x + y == 2020]
+  let result = take 1 $ [ (x, y) | (x, y) <- combinations2 nums, x + y == 2020]
   case listToMaybe result of
     Just (x, y) -> print $ x * y
     Nothing     -> putStrLn "Nope"
