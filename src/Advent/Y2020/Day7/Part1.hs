3f88cd4009696f88c8a693aac7d25884fefbf85c
James King <james@agentultra.com>
2020-12-08 23:54:19 -0500

    Finish solution for Day 7, Part 1

--- src/Advent/Y2020/Day7/Part1.hs
+++ src/Advent/Y2020/Day7/Part1.hs
@@ -1,5 +1,6 @@
 module Advent.Y2020.Day7.Part1 where
 
+import Data.Graph
 import qualified Data.HashMap.Strict as H
 import qualified Data.Set as S
 import qualified Data.Text.IO as T
@@ -7,16 +8,18 @@ import qualified Data.Text.IO as T
 import Advent.Y2020.Day7.Bag
 import Advent.Y2020.Day7.Parse
 
-type BagTree = HashMap Bag Contains
+type BagTree = HashMap Bag [Bag]
 
 bagTree :: [BagNode] -> BagTree
 bagTree nodeList = go H.empty nodeList
   where
     go bagTree [] = bagTree
     go bagTree ((node, nodes):bs) =
-      case H.lookup node bagTree of
-        Nothing -> go (H.insert node nodes bagTree) bs
-        Just _ -> go (H.update (\bs' -> Just $ bs' <> nodes) node bagTree) bs
+      let bags = [ (snd $ n, node) | n <- nodes ]
+          merged = foldl' mergeBags bagTree bags
+      in go merged bs
+      where
+        mergeBags t (k, v) = H.insertWith (<>) k [v] t
 
 findNumBags :: Bag -> BagTree -> Int
 findNumBags b bagTree = length $ go b bagTree
@@ -24,10 +27,8 @@ findNumBags b bagTree = length $ go b bagTree
     go :: Bag -> BagTree -> Set Bag
     go b' t =
       case H.lookup b' t of
-        Nothing   -> S.empty
-        Just bags ->
-          let bags' = map snd bags
-          in S.unions (S.fromList bags' : map (`go` t) bags')
+        Nothing    -> S.empty
+        Just bags' -> S.unions (S.fromList bags' : map (`go` t) bags')
 
 solution :: IO ()
 solution = do
