adec3bebd2125da6c355133a51d79101dbec50d6
James King <james@agentultra.com>
2020-12-09 00:29:56 -0500

    Add solution for Day 7, Part 2

--- src/Advent/Y2020/Day7/Part2.hs
+++ src/Advent/Y2020/Day7/Part2.hs
@@ -1,4 +1,19 @@
 module Advent.Y2020.Day7.Part2 where
 
+import qualified Data.HashMap.Strict as H
+import qualified Data.Text.IO as T
+
+import Advent.Y2020.Day7.Parse
+import Advent.Y2020.Day7.Bag
+
+costOfBags :: Text -> HashMap Bag Contains -> Int
+costOfBags bag tree = sum $ map countBags $ H.lookupDefault [] bag tree
+  where
+    countBags (num, b) = num * (1 + costOfBags b tree)
+
 solution :: IO ()
-solution = putStrLn "Not implemented yet"
+solution = do
+  input <- T.readFile "data/2020/Day7.txt"
+  case parseInput input of
+    Left err       -> putStrLn err
+    Right bagNodes -> print $ costOfBags "shiny gold" $ H.fromList bagNodes
