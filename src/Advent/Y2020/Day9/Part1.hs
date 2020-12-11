3a3f9232444526d2609158dfe0be38497d75da1a
James King <james@agentultra.com>
2020-12-11 13:15:08 -0500

    Add solution for Day 9 part 1

--- src/Advent/Y2020/Day9/Part1.hs
+++ src/Advent/Y2020/Day9/Part1.hs
@@ -1,4 +1,33 @@
 module Advent.Y2020.Day9.Part1 where
 
+import Advent.Input
+
+-- | Collect a value from the list and the _n_ values preceding it
+prior :: Int -> [a] -> [(a, [a])]
+prior n
+  = catMaybes
+  . map uncons
+  . filter (\l -> length l == (n + 1))
+  . map (reverse . (\l -> drop (length l - (n + 1)) l))
+  . inits
+
+combinations2 :: [a] -> [(a, a)]
+combinations2 xs = [(x, y) | (x:ys) <- tails xs, y <- ys]
+
+sum2 :: Num a => (a, a) -> a
+sum2 (x, y) = x + y
+
+validXmas :: (Int, [Int]) -> Either Int Int
+validXmas (target, nums) = case find ((== target) . sum2) $ combinations2 nums of
+  Nothing -> Left target
+  Just _  -> Right target
+
+findInvalidXmas :: [Int] -> Maybe Int
+findInvalidXmas = listToMaybe . lefts . map validXmas . prior 25
+
 solution :: IO ()
-solution = putStrLn "Not implemented yet"
+solution = do
+  input <- readInput "data/2020/Day9.txt"
+  case traverse readInt input of
+    Left err   -> putStrLn err
+    Right nums -> print $ maybe (-1) identity $ findInvalidXmas nums
