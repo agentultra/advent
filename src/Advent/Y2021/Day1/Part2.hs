890b04b289dcfdfd2d3f29246b93f20e447218f3
James King <james@agentultra.com>
2021-12-01 22:05:47 -0500

    Add Day1 Part 2

--- src/Advent/Y2021/Day1/Part2.hs
+++ src/Advent/Y2021/Day1/Part2.hs
@@ -1,4 +1,15 @@
 module Advent.Y2021.Day1.Part2 where
 
+import qualified Data.Text.IO as T
+
+import Advent.Y2021.Day1.Parse
+import Advent.Y2021.Day1.Sonar
+
 solution :: IO ()
-solution = putStrLn "Not implemented yet"
+solution = do
+  raw <- T.readFile "data/2021/Day1.txt"
+  case parseInput raw of
+    Left err -> print err
+    Right readings -> do
+      let s = part2Solution readings
+      print s
--- src/Advent/Y2021/Day1/Sonar.hs
+++ src/Advent/Y2021/Day1/Sonar.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Advent.Y2021.Day1.Sonar where
 
 import qualified Data.List.NonEmpty as NE
 
 newtype Reading = Reading { getReading :: Int }
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Num, Ord, Show)
 
 data Delta
   = Increased Reading Reading
@@ -17,6 +19,13 @@ adjacentDeltas (x :| xs) =
   let xs' = x : xs
   in NoPrevious x : [ delta y y' | (y, y') <- zip xs' $ tail (NE.fromList xs')]
 
+windowReadings :: [Reading] -> NonEmpty Reading
+windowReadings
+  = NE.fromList
+  . map (sum . take 3)
+  . filter (\t -> length t >= 3)
+  . tails
+
 delta :: Reading -> Reading -> Delta
 delta r1 r2 = case compare r1 r2 of
   LT -> Increased r1 r2
@@ -29,3 +38,6 @@ part1Solution = foldl' countIncreased 0 . adjacentDeltas
     countIncreased :: Int -> Delta -> Int
     countIncreased count (Increased _ _ ) = count + 1
     countIncreased count _ = count
+
+part2Solution :: [Reading] -> Int
+part2Solution = part1Solution . windowReadings
