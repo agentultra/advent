2c55a34aca0a5393ca8d03f7cea95f6d69765091
James King <james@agentultra.com>
2021-12-02 19:18:32 -0500

    Add 2021-Day2-Part2 solution

--- src/Advent/Y2021/Day2/Part2.hs
+++ src/Advent/Y2021/Day2/Part2.hs
@@ -1,4 +1,13 @@
 module Advent.Y2021.Day2.Part2 where
 
+import qualified Data.Text.IO as T
+
+import Advent.Y2021.Day2.Parse
+import Advent.Y2021.Day2.Sub
+
 solution :: IO ()
-solution = putStrLn "Not implemented yet"
+solution = do
+  raw <- T.readFile "data/2021/Day2.txt"
+  case parseInput raw of
+    Left err -> print err
+    Right cmds -> print $ part2Solution . fromList $ cmds
--- src/Advent/Y2021/Day2/Sub.hs
+++ src/Advent/Y2021/Day2/Sub.hs
@@ -13,23 +13,35 @@ data Command = Command Direction Int
 
 data Submarine
   = Submarine
-  { submarineX :: Int
+  { submarineX   :: Int
   -- ^ 0 .. n goes from left to right
-  , submarineY :: Int
+  , submarineY   :: Int
   -- ^ 0 .. n goes from the surface to the depths
+  , submarineAim :: Int
   }
   deriving (Eq, Show)
 
-navigate :: NonEmpty Command -> Submarine
-navigate = foldl' moveSub initSub
+navigate :: (Submarine -> Command -> Submarine) -> NonEmpty Command -> Submarine
+navigate f = foldl' f initSub
   where
-    initSub = Submarine 0 0
-    moveSub :: Submarine -> Command -> Submarine
-    moveSub (Submarine x y) (Command Forward amt) = Submarine (x + amt) y
-    moveSub (Submarine x y) (Command Down amt)    = Submarine x (y + amt)
-    moveSub (Submarine x y) (Command Up amt)      = Submarine x (y - amt)
+    initSub = Submarine 0 0 0
+
+moveSub :: Submarine -> Command -> Submarine
+moveSub (Submarine x y a) (Command Forward amt) = Submarine (x + amt) y a
+moveSub (Submarine x y a) (Command Down amt)    = Submarine x (y + amt) a
+moveSub (Submarine x y a) (Command Up amt)      = Submarine x (y - amt) a
+
+aimSub :: Submarine -> Command -> Submarine
+aimSub (Submarine x y a) (Command Down amt)    = Submarine x y (a + amt)
+aimSub (Submarine x y a) (Command Up amt)      = Submarine x y (a - amt)
+aimSub (Submarine x y a) (Command Forward amt) = Submarine (x + amt) (y + (a * amt)) a
 
 part1Solution :: NonEmpty Command -> Int
 part1Solution cmds =
-  let (Submarine x y) = navigate cmds
+  let (Submarine x y _) = navigate moveSub cmds
+  in x * y
+
+part2Solution :: NonEmpty Command -> Int
+part2Solution cmds =
+  let (Submarine x y _) = navigate aimSub cmds
   in x * y
