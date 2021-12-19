2efec30a23c647ae4697302527795f796f1ec6f6
James King <james@agentultra.com>
2021-12-19 16:12:28 -0500

    Refactor src/Advent/Y2021/Day9/Vents.hs
    Lift the 'neighbours' and `lowPoints` functions up to make
    implementing Part 2 easier.

--- src/Advent/Y2021/Day9/Vents.hs
+++ src/Advent/Y2021/Day9/Vents.hs
@@ -7,26 +7,28 @@ import qualified Advent.Grid as G
 
 type CaveFloor = Grid Int
 
-neighbours :: CaveFloor -> Int -> Int -> [Int]
-neighbours flr x y =
-  catMaybes [ G.get flr (x-1) y
-            , G.get flr x (y-1)
-            , G.get flr (x+1) y
-            , G.get flr x (y+1)
-            ]
+floorNeighbours :: CaveFloor -> Int -> Int -> [Int]
+floorNeighbours flr x = mapMaybe (uncurry (G.get flr)) . neighbours x
 
-lowPoints :: CaveFloor -> [Int]
+neighbours :: Int -> Int -> [(Int, Int)]
+neighbours x y = [ (x-1, y)
+                 , (x, y - 1)
+                 , (x+1, y)
+                 , (x, y+1)
+                 ]
+
+lowPoints :: CaveFloor -> [(Int, Int)]
 lowPoints flr@(Grid w h _) = do
   x <- [0..w-1]
   y <- [0..h-1]
   case G.get flr x y of
     Nothing -> error $ T.pack "WAT"
     Just c ->
-      if all (> c) $ neighbours flr x y
-      then pure c
+      if all (> c) $ floorNeighbours flr x y
+      then pure (x, y)
       else []
 
 part1Solution :: NonEmpty (NonEmpty Int) -> Int
 part1Solution xs = case G.mkGrid . map toList . toList $ xs of
   Nothing -> error $ T.pack "Bad Input"
-  Just g  -> sum . map (+1) . lowPoints $ g
+  Just g  -> sum . mapMaybe (fmap (+1) . uncurry (G.get g)) . lowPoints $ g
