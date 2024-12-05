c21a235a03c563874d5f24bc4d9941afa56902cc
James King <james@mercury.com>
2024-12-05 01:30:36 -0500

    Add 2024 - Day 4 Part 1
    It's giving the wrong answer, I checked with someone else's script
    which gave the right answer.  I don't actually have the star yet on
    this one.
    
    It's tested to heck, I'm not sure where the problem is.
    
    My original attempt didn't work because the diagonals I was taking on
    the matrix weren't relative to the position of the X which some of the
    examples revealed.
    
    However the examples both pass with this solution as do all of my
    other examples trying to suss out the edge case.

--- src/Advent/Y2024/Day4/Part1.hs
+++ src/Advent/Y2024/Day4/Part1.hs
@@ -1,4 +1,90 @@
 module Advent.Y2024.Day4.Part1 where
 
+import Advent.Grid (Grid)
+import qualified Advent.Grid as Grid
+import Advent.Input
+import Data.Maybe
+import qualified Data.Text as T
+
+import qualified Debug.Trace as Debug
+
+xmas :: String
+xmas = "XMAS"
+
+hits
+  :: (Char -> Bool)
+  -> ((Int, Int) -> Grid Char -> Int)
+  -> Grid Char
+  -> Int
+hits target search grid
+  = let counts = [ search (x, y) grid
+                 | x <- [0..(Grid._gridWidth grid)-1]
+                 , y <- [0..(Grid._gridHeight grid)-1]
+                 , target . fromJust $ Grid.get grid x y
+                 ]
+    in sum counts
+
+findXmas :: (Int, Int) -> Grid Char -> Int
+findXmas ix grid
+  = north ix grid
+  + northE ix grid
+  + east ix grid
+  + southE ix grid
+  + south ix grid
+  + southW ix grid
+  + west ix grid
+  + northW ix grid
+
+north :: (Int, Int) -> Grid Char -> Int
+north (x, y) grid
+  = if catMaybes [ Grid.get grid x (y-i) | i <- [0..3] ] == xmas
+    then 1
+    else 0
+
+northE :: (Int, Int) -> Grid Char -> Int
+northE (x, y) grid
+  = if catMaybes [ Grid.get grid (x+i) (y-i) | i <- [0..3] ] == xmas
+    then 1
+    else 0
+
+east :: (Int, Int) -> Grid Char -> Int
+east (x, y) grid
+  = if catMaybes [ Grid.get grid (x+i) y | i <- [0..3] ] == xmas
+    then 1
+    else 0
+
+southE :: (Int, Int) -> Grid Char -> Int
+southE (x, y) grid
+  = if catMaybes [ Grid.get grid (x+i) (y+i) | i <- [0..3] ] == xmas
+    then 1
+    else 0
+
+south :: (Int, Int) -> Grid Char -> Int
+south (x, y) grid
+  = if catMaybes [ Grid.get grid x (y+i) | i <- [0..3] ] == xmas
+    then 1
+    else 0
+
+southW :: (Int, Int) -> Grid Char -> Int
+southW (x, y) grid
+  = if catMaybes [ Grid.get grid (x-i) (y+i) | i <- [0..3] ] == xmas
+    then 1
+    else 0
+
+west :: (Int, Int) -> Grid Char -> Int
+west (x, y) grid
+  = if catMaybes [ Grid.get grid (x-i) y | i <- [0..3] ] == xmas
+    then 1
+    else 0
+
+northW :: (Int, Int) -> Grid Char -> Int
+northW (x, y) grid
+  = if catMaybes [ Grid.get grid (x-i) (y-i) | i <- [0..3] ] == xmas
+    then 1
+    else 0
+
 solution :: IO ()
-solution = putStrLn "Not implemented yet"
+solution = do
+  raw <- readInput "data/2024/Day4.txt"
+  let grid = fromJust . Grid.mkGrid . map T.unpack $ raw
+  print $ hits (== 'X') findXmas grid
--- test/Advent/Y2024/Day4/Part1Spec.hs
+++ test/Advent/Y2024/Day4/Part1Spec.hs
@@ -2,10 +2,127 @@ module Advent.Y2024.Day4.Part1Spec where
 
 import Test.Hspec
 
+import Advent.Grid
 import Advent.Y2024.Day4.Part1
+import Data.Maybe
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+
+smallGrid :: Grid Char
+smallGrid
+  = fromJust . mkGrid $
+  [ "..X..."
+  , ".SAMX."
+  , ".A..A."
+  , "XMAS.S"
+  , ".X...."
+  ]
+
+largerGrid :: Grid Char
+largerGrid
+  = fromJust . mkGrid $
+  [ "MMMSXXMASM"
+  , "MSAMXMSMSA"
+  , "AMXSXMAAMM"
+  , "MSAMASMSMX"
+  , "XMASAMXAMM"
+  , "XXAMMXXAMA"
+  , "SMSMSASXSS"
+  , "SAXAMASAAA"
+  , "MAMMMXMMMM"
+  , "MXMXAXMASX"
+  ]
+
+crazyGrid :: Grid Char
+crazyGrid
+  = fromJust . mkGrid $
+  [ "S..S..S"
+  , ".A.A.A."
+  , "..MMM.."
+  , "SAMXMAS"
+  , "..MMM.."
+  , ".A.A.A."
+  , "S..S..S"
+  ]
+
+crossGrid :: Grid Char
+crossGrid
+  = fromJust . mkGrid $
+  [ "X.X."
+  , ".MM."
+  , "XMAS"
+  , "..SS"
+  ]
 
 spec :: Spec
 spec = do
-  describe "Advent - Y2024 -  - Part 1" $ do
-    it "Not Implemented" $ do
-      pendingWith "Not implemented"
+  fdescribe "Advent - Y2024 -  - Part 1" $ do
+    describe "north" $ do
+      context "Given the small example" $ do
+        it "should find XMAS at 1,4" $ do
+          north (1, 4) smallGrid `shouldBe` 1
+        it "should not find XMAS at 0,4" $ do
+          north (0, 4) smallGrid `shouldBe` 0
+    describe "south" $ do
+      context "Given the larger example" $ do
+        it "should find XMAS at 9,4" $ do
+          south (9,3) largerGrid `shouldBe` 1
+    describe "west" $ do
+      context "Given the small example" $ do
+        it "should find XMAS at 4,1" $ do
+          west (4, 1) smallGrid `shouldBe` 1
+        it "should not find XMAS at 0,3" $ do
+          west (0, 3) smallGrid `shouldBe` 0
+        it "should not find XMAS at 3,3" $ do
+          west (3, 3) smallGrid `shouldBe` 0
+    describe "east" $ do
+      context "Given the small example" $ do
+        it "should find XMAS at 0,3" $ do
+          east (0, 3) smallGrid `shouldBe` 1
+    describe "southE" $ do
+      context "Given the small example" $ do
+        it "should find XMAS at 2,0" $ do
+          southE (2, 0) smallGrid `shouldBe` 1
+    describe "southW" $ do
+      context "Given the larger example" $ do
+        it "should find XMAS at 9,3" $ do
+          southW (9, 3) largerGrid `shouldBe` 1
+    describe "northW" $ do
+      context "Given the larger example" $ do
+        it "should find XMAS at 9,9" $ do
+          northW (9, 9) largerGrid `shouldBe` 1
+    describe "northE" $ do
+      context "Given the larger example" $ do
+        it "should find XMAS at 0,5" $ do
+          northE (0, 5) largerGrid `shouldBe` 1
+        it "should not find XMAS at 0,9" $ do
+          northE (0, 9) largerGrid `shouldBe` 0
+        it "should not find anything out of bounds" $ do
+          northE (10, 10) largerGrid `shouldBe` 0
+
+    describe "findXmas" $ do
+      context "Given the larger example" $ do
+        it "should count 2 XMAS at 9,3" $ do
+          findXmas (9, 3) largerGrid `shouldBe` 2
+        it "should count 1 at 0,5" $ do
+          findXmas (0, 5) largerGrid `shouldBe` 1
+      context "Given the crazy grid" $ do
+        it "should count 8 XMAS at 3,3" $ do
+          findXmas (3, 3) crazyGrid `shouldBe` 8
+
+    describe "hits" $ do
+      context "Given the small example" $ do
+        it "should find 4 XMAS" $ do
+          hits (== 'X') findXmas smallGrid `shouldBe` 4
+      context "Given the larger example" $ do
+        it "should find 18 XMAS" $ do
+          hits (== 'X') findXmas largerGrid `shouldBe` 18
+      context "Given the crazy grid" $ do
+        it "should find 8 XMAS when we target 'X'" $ do
+          hits (== 'X') findXmas crazyGrid `shouldBe` 8
+        it "should find none when we target '.'" $ do
+          hits (== '.') findXmas crazyGrid `shouldBe` 0
+
+      context "Given the cross grid" $ do
+        it "should find 3" $ do
+          hits (== 'X') findXmas crossGrid `shouldBe` 3
